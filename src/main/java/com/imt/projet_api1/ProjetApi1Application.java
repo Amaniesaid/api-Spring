3a1238e41616b7d344f28cefca6e9eec156d90a2
LeCoinDuCellulaire <lecoinducellulaire@gmail.com>
2025-01-15 14:02:59 +0100

    Project Initialization

---
+++ src/main/java/com/imt/projet_api1/ProjetApi1Application.java
@@ -0,0 +1,13 @@
+package com.imt.projet_api1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetApi1Application.class, args);
+	}
+
+}
